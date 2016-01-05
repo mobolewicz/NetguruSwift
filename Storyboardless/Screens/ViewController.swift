86ebccab204c75d6a1eab467e22ac39a4e68dab6
Obolewicz Marcin <ext.marcin.obolewicz@tieto.com>
2016-01-05 17:26:33 +0100

    Auto layout constraints visual formatted added

--- Storyboardless/Screens/ViewController.swift
+++ Storyboardless/Screens/ViewController.swift
@@ -10,15 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var label1: UILabel?
+    var label2: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupLabel()
+        self.setupLabels()
+        self.updateConstraints()
     }
     
-    func setupLabel() {
-        let label = UILabel(frame: self.view.frame)
-        label.text = "Do your stuff"
-        label.center = self.view.center
-        self.view.addSubview(label)
+
+    
+    func setupLabels() {
+        self.label1 = UILabel()
+        self.label1!.text = "Do your stuff"
+        self.label1!.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label1!)
+        
+        self.label2 = UILabel()
+        self.label2!.text = "Or do not, there's no try"
+        self.label2!.translatesAutoresizingMaskIntoConstraints = false
+
+        self.view.addSubview(label2!)
+    }
+    
+    func updateConstraints(){
+        let viewsDictionary =
+        [
+            "label1" : self.label1 as! AnyObject,
+            "label2" : self.label2 as! AnyObject
+        ]
+        
+        var format: String
+        var constraintsArray: [NSLayoutConstraint]
+        
+        format = "H:|-[label1]-[label2]-|"
+        constraintsArray = NSLayoutConstraint.constraintsWithVisualFormat(format, options:NSLayoutFormatOptions(rawValue: 0), metrics:nil, views:viewsDictionary)
+        self.view.addConstraints(constraintsArray)
+        
+        format = "V:|-[label1]-|"
+        constraintsArray = NSLayoutConstraint.constraintsWithVisualFormat(format, options:NSLayoutFormatOptions(rawValue: 0), metrics:nil, views:viewsDictionary)
+        self.view.addConstraints(constraintsArray)
+        
+        format = "V:|-[label2]-|"
+        constraintsArray = NSLayoutConstraint.constraintsWithVisualFormat(format, options:NSLayoutFormatOptions(rawValue: 0), metrics:nil, views:viewsDictionary)
+        self.view.addConstraints(constraintsArray)
+        
     }
 }
